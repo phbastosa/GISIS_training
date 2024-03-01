650f9add547f2ecb5e4e6c46aa3cc89727a58f36
phbastosa <phbastosa@gmail.com>
2024-02-29 22:15:57 -0300

    new tiny project

---
+++ depth_time_conversion/conversions.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+class Conversion():
+    
+    def __build_time_wavelet(self):
+
+        ns = int(4.0*np.pi/self.__fmax/self.__dt)
+
+        self.__wavelet = np.zeros(ns)
+ 
+        fc = self.__fmax / (3.0 * np.sqrt(np.pi))
+        
+        mid = int(0.5*ns)
+        for i in range(-mid, mid):
+            arg = pow(i*self.__dt*fc*np.pi, 2.0)
+            self.__wavelet[i + mid] = (1.0 - 2.0*arg)*np.exp(-arg) 
+
+    def __build_depth_wavelet(self):
+
+        ns = int(0.25*np.pi/self.__fmax/self.__dt)
+
+        self.__wavelet = np.zeros(ns) 
+
+        mid = int(0.5*ns)
+        for i in range(-mid, mid):
+            arg = pow(i*self.__dt*self.__fmax*np.pi**2, 2.0)
+            self.__wavelet[i + mid] = (1.0 - 2.0*arg)*np.exp(-arg) 
+
+    def __build_depth_amplitudes(self):
+
+        reflectivity = np.zeros_like(self.__model)    
+
+        reflectivity[:-1] = (self.__model[1:] - self.__model[:-1]) / (self.__model[1:] + self.__model[:-1]) 
+
+        transmission = np.zeros_like(reflectivity)  
+
+        self.__amp_depth = np.zeros_like(reflectivity)
+
+        self.__amp_depth[0] = reflectivity[0]         
+        transmission[0] = 1 - reflectivity[0]     
+
+        for i in range(1, len(reflectivity)):   
+            self.__amp_depth[i] = transmission[i-1] * reflectivity[i]         
+            transmission[i] = transmission[i-1] * (1 - reflectivity[i]) 
+
+            for j in range(i, 0, -1):  
+                self.__amp_depth[i] *= (1 - reflectivity[i - j])    
+
+    def __build_time_amplitudes(self):
+  
+        t = np.zeros(self.__nx, dtype = int)
+        self.__amp_time = np.zeros((self.__nt, self.__nx))
+    
+        for i in range(self.__nz):
+            t += np.array(2 * (self.__dh / self.__model[i]) / self.__dt, dtype = int)
+            for j in range(self.__nx):
+                if t[j] < self.__nt:
+                    self.__amp_time[t[j],j] = self.__amp_depth[i,j]
+
+    def model_to_time_section(self, velocity_model:np.ndarray, model_spacing:float, total_time:float, time_spacing:float, max_frequency:float) -> np.ndarray:
+        
+        self.__dt = time_spacing
+        self.__dh = model_spacing
+        self.__fmax = max_frequency
+        self.__model = velocity_model
+
+        self.__nt = int(total_time / time_spacing) + 1
+
+        self.__nz, self.__nx = np.shape(self.__model)
+
+        self.__build_time_wavelet()
+        self.__build_depth_amplitudes()
+        self.__build_time_amplitudes()
+
+        self.__time_section = np.zeros_like(self.__amp_time)
+        for j in range(self.__nx):
+            self.__time_section[:,j] = np.convolve(self.__wavelet, self.__amp_time[:,j], "same")
+        
+        return self.__time_section
+    
+    def model_to_depth_section(self, velocity_model:np.ndarray, model_spacing:float, total_time:float, time_spacing:float, max_frequency:float) -> np.ndarray:
+        self.__dt = time_spacing
+        self.__dh = model_spacing
+        self.__fmax = max_frequency
+        self.__model = velocity_model
+
+        self.__nt = int(total_time / time_spacing) + 1
+
+        self.__nz, self.__nx = np.shape(self.__model)
+
+        self.__build_depth_wavelet()
+        self.__build_depth_amplitudes()
+
+        self.__depth_section = np.zeros_like(self.__amp_depth)
+        for j in range(self.__nx):
+            self.__depth_section[:,j] = np.convolve(self.__wavelet, self.__amp_depth[:,j], "same")
+
+        return self.__depth_section
+
+    def time_to_depth():
+        pass
+
+    def depth_to_time():
+        pass
+
---
+++ depth_time_conversion/main.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from conversions import Conversion
+
+def read_binary_matrix(nz, nx, filename):
+    data = np.fromfile(filename, dtype = np.float32, count = nz*nx)
+    return np.reshape(data, [nz, nx], order = 'F')
+
+nx = [681, 601, 801]
+nz = [141, 161, 181]
+
+total_time = 4.0       # [s]
+time_spacing = 1e-3    # [s]
+model_spacing = 25.0   # [m]
+max_frequency = 30.0   # [Hz] 
+
+marmousi_model = read_binary_matrix(nz[0],nx[0],"models/marmousi_141x681_25m.bin")
+salt_dome_model = read_binary_matrix(nz[1],nx[1],"models/salt_dome_161x601_25m.bin")
+overthurst_model = read_binary_matrix(nz[2],nx[2],"models/overthrust_181x801_25m.bin")
+
+converter = Conversion()
+
+marmousi_time_section = converter.model_to_time_section(marmousi_model, model_spacing, total_time, time_spacing, max_frequency)
+marmousi_depth_section = converter.model_to_depth_section(marmousi_model, model_spacing, total_time, time_spacing, max_frequency)
+
+
+
+
+# salt_dome_time_section = m2ts.Model_to_time_section(salt_dome_model, model_spacing, total_time, time_spacing, max_frequency)
+# overthrust_time_section = m2ts.Model_to_time_section(overthurst_model, model_spacing, total_time, time_spacing, max_frequency)
+
+
