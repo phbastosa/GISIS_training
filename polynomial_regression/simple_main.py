5440474ac6461d20dbc559e05874c6bd241ea92e
phbastosa <phbastosa@gmail.com>
2024-03-08 19:12:13 -0300

    simple update

--- polynomial_regression/simple_main.py
+++ polynomial_regression/simple_main.py
@@ -3,31 +3,37 @@ import matplotlib.pyplot as plt
 
 from functions import *
 
-# configuration 
+# configuration -------------------------------------------
+
 N = 1001
 
-p_true = np.array([0.5, 0.7])
+p_true = np.array([0.5, 0.7, -0.2, 0.3])
 
 x = np.linspace(0, 2, N, dtype = float)
 
-# modeling 
-y = build_polynomial_function(p_true, x)
+# modeling ------------------------------------------------
+  
+yt = build_polynomial_function(p_true, x)
 
-yn = add_noise(y, 0.5)
+yn = add_noise(yt, 0.1)
 
-yn[500:700] += 0.5
+yn[500:700] *= 2
 
-# inversion
+# inversion -----------------------------------------------
+
 p_calc = least_squares_solution(x, len(p_true), yn)
 
-# validation
+# validation ----------------------------------------------
+
 yf = build_polynomial_function(p_calc, x)
 
-# results
+# results -------------------------------------------------
+
 fig, ax = plt.subplots(num = "Parameters estimation", figsize = (10,7))
 
-ax.plot(x, yn, "ok", label = f"True: p0 = {p_true[0]:.1f}, p1 = {p_true[1]:.1f}")
-ax.plot(x, yf, "-b", label = f"Calc: p0 = {p_calc[0]:.1f}, p1 = {p_calc[1]:.1f}")
+ax.plot(x, yn, "ok", label = f"Observed data")
+ax.plot(x, yt, "-r", label = f"p_true = {np.around(p_true, decimals = 1)}")
+ax.plot(x, yf, "-b", label = f"p_calc = {np.around(p_calc, decimals = 1)}")
 
 ax.set_xlabel("X", fontsize = 15)
 ax.set_ylabel("Y", fontsize = 15)
