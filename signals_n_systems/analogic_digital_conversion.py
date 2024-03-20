e15196c7d866e7994a4f53e5ca57cf22266809f8
phbastosa <phbastosa@gmail.com>
2024-03-20 15:39:37 -0300

    next friday finished

--- signals_n_systems/analogic_digital_conversion.py
+++ signals_n_systems/analogic_digital_conversion.py
@@ -1,16 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sine_frequencies = [10]
+sine_amplitudes = [6, 10, 2] 
+sine_frequencies = [5, 6, 7]     # [Hz]
 
-total_time = 0.5          # [s]
-sampling_frequency = 100  # [Hz]
+total_time = 1.0                    # [s]
+f_s = 50                            # sampling frequency [Hz]
 
-analogic_time = np.linspace(0, total_time, int(1e6))
+analogic_time = np.linspace(0, total_time, int(1e6)) 
 analogic_sine = np.zeros_like(analogic_time)
 
-for frequency in sine_frequencies:
-    analogic_sine += np.sin(2.0 * np.pi * frequency * analogic_time)  
+for k, freq in enumerate(sine_frequencies):
+    w_0 = 2.0*np.pi*freq
+    analogic_sine += sine_amplitudes[k]*np.sin(w_0*analogic_time)  
 
 ''' 
     Analytical representation of a digital signal
@@ -22,20 +24,20 @@ for frequency in sine_frequencies:
     w_s -> discrete signal angular frequency
 '''
 
-f_0 = np.lcm.reduce(sine_frequencies)  # frequencies mmc
-f_s = sampling_frequency
-
-w_s = 2.0*np.pi*f_0/f_s                # discrete angular frequency
-
 dt = 1.0 / f_s                         # time spacing
 nt = int(total_time / dt)              # total samples 
 
 n = np.arange(nt)                      # samples array
-t = n*dt                               # discrete time array
+discrete_time = n*dt                   # discrete time array
 
-digital_sine = np.sin(w_s * n)
+discrete_sine = np.zeros(nt)
 
-digital_sine_fft = np.fft.fft(digital_sine)
+for k, freq in enumerate(sine_frequencies):
+    w_s = 2.0*np.pi*freq/f_s      
+    discrete_sine += sine_amplitudes[k]*np.sin(w_s * n)  
+
+discrete_sine_fft = np.fft.fft(discrete_sine)
+discrete_sine_fft *= np.max(sine_amplitudes) / np.max(np.abs(discrete_sine_fft))
 
 f = np.fft.fftfreq(nt, dt)             # frequencies array
 
@@ -44,23 +46,24 @@ Nyquist = 0.5*f_s                      # Max frequnecy of a spectrum
 fig, ax = plt.subplots(ncols = 1, nrows = 3, figsize = (15, 9))
 
 ax[0].plot(analogic_time, analogic_sine)
-ax[0].stem(t, digital_sine, markerfmt = "k", linefmt = "k--", basefmt = "k")
+ax[0].stem(discrete_time, discrete_sine, markerfmt = "k", linefmt = "k--", basefmt = "k")
 ax[0].set_xlim([0, total_time])
 ax[0].set_title("Analogic - Digital Conversion", fontsize = 18)
 ax[0].set_xlabel("Time [s]", fontsize = 15)
 ax[0].set_ylabel(r"$x(t)$", fontsize = 15)
 
-ax[1].plot(n, digital_sine, "--o")
+ax[1].plot(n, discrete_sine, "--o")
 ax[1].set_xlim([0, nt-1])
 ax[1].set_title("Discrete Signal", fontsize = 18)
-ax[1].set_xlabel(r"$n$", fontsize = 15)
+ax[1].set_xlabel(r"Discrete time $n$", fontsize = 15)
 ax[1].set_ylabel(r"$x[n]$", fontsize = 15)
 
-ax[2].stem(f, np.abs(digital_sine_fft))
-ax[2].set_xlim([-Nyquist+1,Nyquist-1])
-ax[2].set_title("Discrete Fourier Transform - FFT", fontsize = 18)
+ax[2].stem(f, np.abs(discrete_sine_fft))
+ax[2].set_xlim([1-Nyquist,Nyquist-1])
+ax[2].set_title("Fast Fourier Transform - FFT", fontsize = 18)
 ax[2].set_xlabel("Frequency [Hz]", fontsize = 15)
 ax[2].set_ylabel(r"$X(f)$", fontsize = 15)
 
 fig.tight_layout()
+plt.grid(axis = "y")
 plt.show()
